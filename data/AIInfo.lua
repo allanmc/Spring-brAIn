2ba3fd697ffeffc987cc421d62e54760457ecb94
AllanMC <allanmc@gmail.com>
2009-09-16 15:15:46 +0200

    Added example files

---
+++ data/AIInfo.lua
@@ -0,0 +1,58 @@
+--
+--  Info Definition Table format
+--
+--
+--  These keywords must be lowercase for LuaParser to read them.
+--
+--  key:      user defined or one of the SKIRMISH_AI_PROPERTY_* defines in
+--            SSkirmishAILibrary.h
+--  value:    the value of the property
+--  desc:     the description (could be used as a tooltip)
+--
+--
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+local infos = {
+	{
+		key    = 'shortName',
+		value  = 'brAIn',
+		desc   = 'brAIn',
+	},
+	{
+		key    = 'version',
+		value  = '0.1', -- AI version - !This comment is used for parsing!
+	},
+	{
+		key    = 'name',
+		value  = 'brAIn',
+		desc   = 'brAIn',
+	},
+	{
+		key    = 'description',
+		value  = 'This AI does not do much...',
+		desc   = 'tooltip.',
+	},
+	{
+		key    = 'url',
+		value  = 'http://github.com/allanmc/Spring-brAIn',
+		desc   = 'GitHub Project',
+	},
+	{
+		key    = 'loadSupported',
+		value  = 'no',
+		desc   = 'whether this AI supports loading or not',
+	},
+	{
+		key    = 'interfaceShortName',
+		value  = 'C', -- AI Interface name - !This comment is used for parsing!
+		desc   = 'the shortName of the AI interface this AI needs',
+	},
+	{
+		key    = 'interfaceVersion',
+		value  = '0.1', -- AI Interface version - !This comment is used for parsing!
+		desc   = 'the minimum version of the AI interface this AI needs',
+	},
+}
+
+return infos
