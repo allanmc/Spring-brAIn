7e193fbb402b74037e7c8f0b8df28f46f36dd4ec
Lars Vinther <metalmaniac43@hotmail.com>
2009-09-17 14:16:31 +0200

    GroupController and manager

--- src/GroupController.cpp
+++ src/GroupController.cpp
@@ -1,9 +1,37 @@
 #include "GroupController.h"
 
+
 brainSpace::GroupController::GroupController(void)
 {
+	ConstructionGroupMgr = new ConstructionGroupManager();
+	MilitaryGroupMgr = new MilitaryGroupManager();
 }
 
 brainSpace::GroupController::~GroupController(void)
 {
+}
+
+void brainSpace::GroupController::AddUnit(Unit *unit)
+{
+	if ( unit->GetDef()->IsBuilder() )
+	{
+		ConstructionGroupMgr->AddUnit( unit );
+	}
+	else if ( unit->GetDef()->IsAbleToAttack() )
+	{
+		MilitaryGroupMgr->AddUnit( unit );
+	}
+
+}
+
+void brainSpace::GroupController::RemoveUnit(Unit *unit)
+{
+	if ( unit->GetDef()->IsBuilder() )
+	{
+		ConstructionGroupMgr->RemoveUnit( unit );
+	}
+	else if ( unit->GetDef()->IsAbleToAttack() )
+	{
+		MilitaryGroupMgr->RemoveUnit( unit );
+	}
 }
