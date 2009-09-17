a833da1ec383e36f203c903137b9de7ccd45f301
Lars Vinther <metalmaniac43@hotmail.com>
2009-09-17 14:16:31 +0200

    GroupController and manager

--- src/GroupController.h
+++ src/GroupController.h
@@ -1,13 +1,31 @@
 #ifndef _BRAINSPACE_GROUPCONTROLLER_H
 #define _BRAINSPACE_GROUPCONTROLLER_H
 
+
+
+#include "Unit.h"
+#include "ConstructionGroupManager.h"
+#include "MilitaryGroupManager.h"
+
+
+using namespace springai;
+
+
+
 namespace brainSpace {
-class GroupController
-{
-public:
-	GroupController(void);
-	~GroupController(void);
-};
+	class GroupController
+	{
+	public:
+		GroupController(void);
+		~GroupController(void);
+
+		void AddUnit( Unit* unit );
+		void RemoveUnit( Unit* unit );
+	private:
+		ConstructionGroupManager* ConstructionGroupMgr;
+		MilitaryGroupManager* MilitaryGroupMgr;
+
+	};
 }
 
 #endif // _BRAINSPACE_GROUPCONTROLLER_H
