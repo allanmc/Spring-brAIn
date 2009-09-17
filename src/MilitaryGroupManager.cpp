52bc8a7b646ae7f36cbba90138076fbfcad9caa7
AllanMC <allanmc@gmail.com>
2009-09-17 16:02:35 +0200

    Cleaning up after Lars

--- src/MilitaryGroupManager.cpp
+++ src/MilitaryGroupManager.cpp
@@ -12,24 +12,6 @@ brainSpace::MilitaryGroupManager::~MilitaryGroupManager()
 
 void brainSpace::MilitaryGroupManager::AddUnit( Unit* unit )
 {
-	vector<vector<Unit*>*>::iterator iter;
-	
-	vector<Unit*>* smallestSet = (*UnitGroups.begin());
-
-	if ( UnitGroups.size() == 0 )
-	{
-		return;
-	}
-	for ( iter = UnitGroups.begin() ; iter != UnitGroups.end() ; iter ++ )
-	{
-		vector<Unit*>* currentSet = (*iter);
-		if ( currentSet->size() < smallestSet->size() )
-		{
-			smallestSet = currentSet;
-		}
-	}
-
-	smallestSet->insert( unit );
 }
 
 void brainSpace::MilitaryGroupManager::RemoveUnit( Unit* unit )
