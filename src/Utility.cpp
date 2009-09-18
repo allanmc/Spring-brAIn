aab89e69335effc47c5a16bac541e182460de8c2
AllanMC <allanmc@gmail.com>
2009-09-18 14:49:01 +0200

    experimenting with lua

--- src/Utility.cpp
+++ src/Utility.cpp
@@ -10,6 +10,26 @@ Utility::Utility(AICallback* callback )
 void Utility::ChatMsg(const char* msg, ...)
 {
 	static char c[200];
+
+	/*
+	OptionValues* optionValues = OptionValues::GetInstance(Callback);
+	
+	//optionValues->GetValueByKey("debug");
+	//Utility::ChatMsg(string(optionValues->GetValueByKey("debug")));
+	SNPRINTF(c, 200, "OptionValues getSize: %i", optionValues->GetSize());
+	SSendTextMessageCommand cmd;
+	cmd.text = c;
+	cmd.zone = 0;
+	Callback->GetEngine()->HandleCommand(0, -1, COMMAND_SEND_TEXT_MESSAGE, &cmd);
+	
+	for (int i = 0; i < optionValues->GetSize(); i++) {
+		SNPRINTF(c, 200, "OptionValues debug: %s = %s", optionValues->GetKey(i), optionValues->GetValue(i));
+		cmd.text = c;
+		cmd.zone = 0;
+		Callback->GetEngine()->HandleCommand(0, -1, COMMAND_SEND_TEXT_MESSAGE, &cmd);
+	}
+	*/
+	
 	va_list list;
 	va_start(list, msg);
 	VSNPRINTF(c, 200, msg, list);
@@ -18,6 +38,7 @@ void Utility::ChatMsg(const char* msg, ...)
 	cmd.text = c;
 	cmd.zone = 0;
 	Callback->GetEngine()->HandleCommand(0, -1, COMMAND_SEND_TEXT_MESSAGE, &cmd);
+	
 }
 
 
