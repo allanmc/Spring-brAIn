9f2bb8d25379a10955a3d6936d89c2c9ddf795d8
Jeppe Ravn <IDE@.(none)>
2010-04-15 13:23:53 +0200

    dillerpik

--- rl-learner/src/main.cpp
+++ rl-learner/src/main.cpp
@@ -371,7 +371,7 @@ void LoadConfig(int argc, char *argv[])
 	GAMMA = 0.9f;
 	ALPHA = 0.1f;
 	EPSILON_START = 0.5f;
-	EPSILON_DECAY = 0.99999f;
+	EPSILON_DECAY = 1.0f;
 	CONCURRENT_I = 2;
 	CONCURRENT_T = 3;
 	CONCURRENT_SS = 4;
